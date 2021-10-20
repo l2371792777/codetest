2ee86978036b9a31e75ebf9dd079ef2be759e761
ayanami <l2371792777@163.com>
2021-10-20 20:46:31 +0800

    2021-10-20-20

--- src/main.old.cpp
+++ src/main.old.cpp
@@ -1,5 +1,5 @@
-//...leetcode_476_2021-10-18
-//...https://leetcode-cn.com/problems/number-complement/
+//...leetcode_435_2021-10-20    
+//...https://leetcode-cn.com/problems/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -8,8 +8,7 @@
 using namespace std;
 
 void test();
-int findComplement(int num);
-void pintfBinary(int num);
+
 
 int main()
 {
@@ -19,29 +18,4 @@ int main()
 
 void test()
 {
-    int t = findComplement(0);
-    cout << t;
-}
-
-int findComplement(int num)
-{
-    //低位置一
-    int t = num;
-    t |= t >> 1;
-    t |= t >> 2;
-    t |= t >> 4;
-    t |= t >> 8;
-    t |= t >> 16;
-    return num ^ t;
-}
-
-void pintfBinary(int num)
-{
-    cout << num << " | ";
-    while (num > 1)
-    {
-        cout << num % 2;
-        num /= 2;
-    }
-    cout << num << endl;
 }
